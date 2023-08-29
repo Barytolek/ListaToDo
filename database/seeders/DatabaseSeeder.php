f8bd367692cadccd54f98d653cbd256ea9708740
Barytolek <barytol62@gmail.com>
2023-08-29 02:20:10 +0200

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,16 +10,17 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
-        $int= mt_rand(1262055681,1262055681);
-        $int2= mt_rand(1262055681,1262055681);
-        $string = date("Y-m-d",$int);
-        $string2 = date("Y-m-d",$int2);
-        DB::table('zadania')->insert([
-            'nazwa_zadania' =>'Zadanie '.random_int(1,10),
-            'czy_zakonczone'=> random_int(0,1),
-            'data_dodania' => $string,
-            'data_zakonczenia' => $string2,
-            'opis_zadania' => Str::random(rand(1,35)).' '.Str::random(rand(1,35)).' '.Str::random(rand(1,35)).' '.Str::random(rand(1,35)).' '.Str::random(rand(1,35)).' '.Str::random(rand(1,35)).' '.Str::random(rand(1,35)).' '.Str::random(rand(1,35))
-        ]);
+        for($i=0;$i<3;$i++) {
+            $date =rand(1262055681, 2024654585);
+            $string = date("Y-m-d", $date);
+            $string2 = date("Y-m-d", $date);
+            DB::table('zadania')->insert([
+                'nazwa_zadania' => 'Zadanie ' . random_int(1, 10),
+                'czy_zakonczone' => random_int(0, 1),
+                'data_dodania' => $string,
+                'data_zakonczenia' => $string2,
+                'opis_zadania' => Str::random(rand(1, 35)) . ' ' . Str::random(rand(1, 35)) . ' ' . Str::random(rand(1, 35)) . ' ' . Str::random(rand(1, 35)) . ' ' . Str::random(rand(1, 35)) . ' ' . Str::random(rand(1, 35)) . ' ' . Str::random(rand(1, 35)) . ' ' . Str::random(rand(1, 35))
+            ]);
+        }
     }
 }
