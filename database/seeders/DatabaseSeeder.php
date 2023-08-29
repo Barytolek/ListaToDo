e350474727cb619bb149be62f8123a34011926d6
Barytolek <barytol62@gmail.com>
2023-08-29 02:05:37 +0200

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,24 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $int= mt_rand(1262055681,1262055681);
+        $int2= mt_rand(1262055681,1262055681);
+        $string = date("Y-m-d",$int);
+        $string2 = date("Y-m-d",$int2);
+        DB::table('zadania')->insert([
+            'nazwa_zadania' =>'Zadanie '.random_int(1,10),
+            'czy_zakonczone'=> random_int(0,1),
+            'data_dodania' => $string,
+            'data_zakonczenia' => $string2,
+            'opis_zadania' => Str::random(rand(1,35)).' '.Str::random(rand(1,35)).' '.Str::random(rand(1,35)).' '.Str::random(rand(1,35)).' '.Str::random(rand(1,35)).' '.Str::random(rand(1,35)).' '.Str::random(rand(1,35)).' '.Str::random(rand(1,35))
+        ]);
     }
 }
